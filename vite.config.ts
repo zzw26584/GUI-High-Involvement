99f8abb55e6dd1389d600959e38ef7cfa339ca65
zzw26584 <nguyenhuongrvkns6163@gmail.com>
2025-12-30 20:08:46 +0800

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,11 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  define: {
+    // 关键：将系统环境变量映射到前端可用的 process.env
+    'process.env.API_KEY': JSON.stringify(process.env.API_KEY),
+    'process.env': process.env,
+  },
   server: {
     port: 3000,
   },
